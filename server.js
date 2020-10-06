3825775f5e85ef1b36176e7e01f60d0091d9ce38
Deepthy Nair <dpthynair@gmail.com>
2020-10-06 12:26:58 +0200

    Implemented database

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const connectDb = require('./config/db')
 const app = express()
 
+
+//Connect Db
+connectDb()
 
 app.get('/', (req,res) =>
  res.json({msg: 'Welcome to the contact keeper API ...'}))
